1cea92536fe87f0747126394dd240b2459442233
Riken0306 <68332451+Riken0306@users.noreply.github.com>
2020-10-01 07:48:00 +0530

    Update color.c

--- color.c
+++ color.c
@@ -2,7 +2,9 @@
 int main()
 {
 	int n,i,j=0,k=0,a[100],count=0,v=0;
+	printf("Enter the number of students Rita asked\n");
 	scanf("%d",&n);
+	printf("Enter the answers given by each student");
 	for(i=0;i<n;i++)
 	{
 	    scanf("%d",&a[i]);
@@ -27,7 +29,7 @@ int main()
 	k=a[i];
 	}
 	
-	printf("%d",count);
+	printf("Total number of students present in class=%d",count);
 	
 	
 	return 0;
